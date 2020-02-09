64b5f834bae3b35aab5ee357c9d8c172a81521cf
GzhuFrank <2521012670@qq.com>
2020-02-09 19:28:06 +0800

    init

---
+++ 2Y0A21.c
@@ -0,0 +1,121 @@
+﻿/**
+ ****************************************************   
+ * @�ļ���2Y0A21.c
+ * @���ߣ�������
+ * @�汾��V2.2
+ * @���ڣ�2018��7��
+ * @��飺���ļ���������������ģ��
+ *****************************************************
+ */
+
+#include "2Y0A21.h"
+#include "crc.h"
+#include "usart.h"
+
+int infraredFlag = 0; //����һ��ȫ�ֱ��������ڱ�־��Ļ��Ϩ���뿪����0������Ϩ��1������������
+
+/**
+  * @���ܣ�  ����Adcͨ�����ŵĳ�ʼ��
+  * @������  ��
+  * @����ֵ����
+  */
+void Adc_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	ADC_CommonInitTypeDef ADC_CommonInitStructure;
+	ADC_InitTypeDef ADC_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //ʹ��GPIOAʱ��
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);  //ʹ��ADC1ʱ��
+
+	//==============��ʼ��PA0 IO��=============================//
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;	 //ģ������
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;		 //ͨ��0
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL; //����������
+	GPIO_Init(GPIOA, &GPIO_InitStructure);			 //��ʼ������
+	//===========================================================//
+
+	RCC_APB2PeriphResetCmd(RCC_APB2Periph_ADC1, ENABLE);  //APB2�����ϵ�����ADC1ʱ�ӵĸ�λ
+	RCC_APB2PeriphResetCmd(RCC_APB2Periph_ADC1, DISABLE); //��λ����
+
+	//==����ADC��ͨ�ÿ��ƼĴ���CCR������ADC����ʱ�ӷ�Ƶ��ģʽΪ����ģʽ==========================//
+	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;		 //DMAʧ��
+	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;					 //����ģʽ
+	ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div4;					 //Ԥ��Ƶ4��Ƶ��ADCCLK=PCLK2/4=84/4=21MHz,ADCʱ����õ���36MHz
+	ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles; //���������׶�֮����ӳ�5��ʱ��
+	ADC_CommonInit(&ADC_CommonInitStructure);									 //��ʼ��CCR�Ĵ���
+	//=============================================================================================//
+
+	//===��ʼ��ADC1����������ADC1��ת���ֱ��ʣ�ת����ʽ�����뷽ʽ���Լ��������е������Ϣ��=======//
+	//����Ϊ����ת��������Ҫʹ�õ�DMA������ת��ͨ����Ҫʹ�õ�DMA
+	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;							//12λģʽ
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;						//�Ҷ���
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None; //��ֹ������⣬ʹ����������
+	ADC_InitStructure.ADC_NbrOfConversion = 1;									//����һ��ת������ת���ڹ���������
+	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;						//12λģʽ
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE;								//ʧ�ܷ�ɨ��ģʽ
+	ADC_Init(ADC1, &ADC_InitStructure);											//����ADC��ز����ĳ�ʼ��
+	//=============================================================================================//
+
+	ADC_Cmd(ADC1, ENABLE); //����ADC��ʼ��
+}
+
+/**
+  * @���ܣ�  ��ȡADCֵ
+  * @������  ch������ͨ��ֵ����ΧΪADC_Channel_0~ADC_Channel_15��
+  * @����ֵ���ڹ���ͨ���л�ȡADC1ת���Ľ��
+  */
+u16 Get_Adc(u8 ch)
+{
+	ADC_RegularChannelConfig(ADC1, ch, 1, ADC_SampleTime_480Cycles); //����ADC1�Ĺ���ͨ���飬����ʱ��Ϊ480������
+	ADC_SoftwareStartConv(ADC1);									 //ʹ��ָ����ADC1������ת����������
+	while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC))
+		; //��ADת���У����ADC1�ı�־λ�Ƿ����ã����ҵȴ��������
+	return ADC_GetConversionValue(ADC1);
+}
+
+/**
+  * @���ܣ�  ���ж�β����������ƽ��ֵ
+  * @������  �����Ĵ���
+  * @����ֵ���������ADCƽ��ֵ
+  */
+u16 Get_Adc_Average(u8 ch, u8 times)
+{
+	u32 temp_val = 0;
+	u8 t;
+	for (t = 0; t < times; t++)
+	{
+		temp_val += Get_Adc(ch);
+		delay_ms(5);
+	}
+	return temp_val / times;
+}
+
+/**
+  * @���ܣ�  ��������ļ�ؽ��ʵʱ��������׿��
+  * @������  ��
+  * @����ֵ����
+  * @ע�⣺  ����⵽���˿���ʱ��������֪ͨ��׿�������������뿪��ʱ��������֪ͨ��׿��Ϣ��
+  */
+void sendInfraredResult(void)
+{
+	u16 adcx;
+	float temp;
+	adcx = Get_Adc_Average(ADC_Channel_0, 20); //��ȡͨ��5��ת��ֵ��20��ȡƽ����ADC�������ԭʼֵ
+	temp = (float)adcx * (3.3 / 4096);		   //��ȡ�����Ĵ�С����ʵ�ʵ�ѹֵ������3.1111
+	delay_ms(250);
+	if (infraredFlag == 0 && temp > (float)1.25) //����ĻϨ�������£���⵽���˿������������ݰ�֪ͨ��׿��������
+	{
+		//printf("screen On\r\n");
+		infraredPackageTxFun(0xaa);
+		doRcvResponResutl(0xaa);
+		infraredFlag = 1; //��Ļ��־λ��1��ʾ������
+	}
+	if (infraredFlag == 1 && temp <= (float)1.25) //����Ļ����������£���⵽����Զ�룬�������ݰ�֪ͨ��׿��Ϩ����
+	{
+		//printf("screen Off\r\n");
+		infraredPackageTxFun(0xbb);
+		doRcvResponResutl(0xbb);
+		infraredFlag = 0; //��Ļ��־λ��0��ʾϨ����
+	}
+}
---
+++ EC35AH.c
@@ -0,0 +1,188 @@
+﻿/**
+ ****************************************************   
+ * @文件：exti.c
+ * @作者：方宗武
+ * @版本：V2.2
+ * @日期：2018年7月
+ * @简介：该文件用于驱动旋转编码器
+ *****************************************************
+ */
+
+#include "EC35AH.h"
+#include "exti.h"
+#include "delay.h" 
+#include "key.h"
+#include "usart.h"
+#include "crc.h"
+#include "EC35AH.h"
+
+static int i = 0;
+static int j = 0;
+static int k = 0;
+
+int old_status = 10;	
+int new_status = 10;
+int adjuest = 10;
+
+//步进值为5，16~32总共33个数。
+extern uint16_t temp_set[33];
+						  
+u8 set_para = 0;
+
+/*
+** 功能：  对EC35AH旋转编码器进行初始化处理
+** 参数：  无
+** 返回值：无
+** 注意：  初始化的引脚分别为 PC10,PC11,PC12
+*/
+void knob_Init(void)
+{
+	
+  GPIO_InitTypeDef  GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+ 
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12; 
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;//普通输入模式
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100M
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOC, &GPIO_InitStructure);//初始化GPIOC
+} 
+void knob_statues_init(void)
+{
+//============================================
+			if(knob_PC10 == 0)
+			{	
+				//	printf("knob_PC10 press\r\n");
+					//printf();
+					old_status = 0;
+			}
+			if(knob_PC11 == 0)
+			{	
+				//	printf("knob_PC11 press\r\n");
+				  old_status = 1;
+			}
+			if(knob_PC12 == 0)
+			{	
+				//	printf("knob_PC12 press\r\n");
+				  old_status = 4;
+			}
+//===========================================
+}
+
+u8 knob_adjust(void)
+{
+		//=================================		
+			if(knob_PC10 == 0 && i!=2)
+			{	
+					i = 1;
+			}
+			if(knob_PC10 == 1)
+			{	
+					i = 0;
+			}
+//==============================
+			if(knob_PC11 == 0&& j!=2)
+			{	
+					j = 1;
+			}
+			if(knob_PC11 == 1)
+			{	
+					j = 0;
+			}
+//===============================
+			if(knob_PC12 == 0&& k!=2)
+			{	
+					k = 1;
+			}
+			if(knob_PC12 == 1)
+			{	
+					k = 0;
+			}
+//==========================================================		
+			if(knob_PC10 == 0 && i == 1)
+			{	
+				//	printf("knob_PC10 press\r\n");
+					//printf();
+					i = 2;
+				new_status = 0;
+				adjuest =  new_status-old_status;
+				if(adjuest==3 || adjuest==1 || adjuest==-4 )
+				{
+					//	printf("contrarotate   left\r\n");
+					set_para = 1;
+					  nixieTubeShowCtl(set_para);
+					//	return 1;
+				}
+
+				else if(adjuest==-1 || adjuest==3 || adjuest==4)
+				{
+						//	printf("contrariwise \r\n");
+						//	nixieTubeShowCtl();
+							
+						//	return 2;
+				}
+
+				old_status = new_status;
+			}
+			if(knob_PC11 == 0 && j == 1)
+			{	
+				//	printf("knob_PC11 press\r\n");
+				  j = 2;
+				new_status = 1;
+				adjuest =  new_status-old_status;
+				if(adjuest==3 || adjuest==1 || adjuest==-4 )
+				{
+					  	set_para = 1;
+					    nixieTubeShowCtl(set_para);
+					//	printf("contrarotate  left\r\n");
+					//	return 1;
+				}
+
+				else if(adjuest==-1 || adjuest==3 || adjuest==4)
+				{
+							//	printf("contrariwise\r\n");
+								//return 2;
+				}
+
+				old_status = new_status;
+			}
+			if(knob_PC12 == 0&& k == 1)
+			{	
+				//	printf("knob_PC12 press\r\n");
+				  k = 2;
+				
+				new_status = 4;
+				adjuest =  new_status-old_status;
+				if(adjuest==3 || adjuest==1 || adjuest==-4 )
+				{
+						set_para = 1;
+					  nixieTubeShowCtl(set_para);
+						//printf("contrarotate  left\r\n");
+					//	return 1;
+				}
+
+				else if(adjuest==-1 || adjuest==3 || adjuest==4)
+				{
+						//printf("contrariwise\r\n");
+						//return 2;
+				}
+
+				old_status = new_status;
+			}
+//==============================================
+		//	return 0;
+			
+
+}
+
+
+
+
+
+
+
+
+
+
+
